fbaa3abb2b4b43d1861e716729b6160112702223
soobin-k <tnqls27qks@naver.com>
2021-10-16 17:41:17 +0900

    Add SignIn AutoLayout

---
+++ Albazip/Source/Start/Login/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  Albazip
+//
+//  Created by 김수빈 on 2021/10/16.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
