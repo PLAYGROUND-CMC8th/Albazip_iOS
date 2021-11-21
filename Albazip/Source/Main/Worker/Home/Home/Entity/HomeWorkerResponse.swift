c22dce3aa6daaf1f6cc2e7064031c6b5c5132838
soobin-k <tnqls27qks@naver.com>
2021-11-21 18:10:46 +0900

    Add Home Compelete People PopUp Scroll

---
+++ Albazip/Source/Main/Worker/Home/Home/Entity/HomeWorkerResponse.swift
@@ -0,0 +1,8 @@
+//
+//  HomeWorkerResponse.swift
+//  Albazip
+//
+//  Created by 김수빈 on 2021/11/21.
+//
+
+import Foundation
--- Albazip/Source/Main/Worker/Home/TodayWork/Cell/HomeWorkerPublicWorkCompleteHeaderTableViewCell.swift
+++ Albazip/Source/Main/Worker/Home/TodayWork/Cell/HomeWorkerPublicWorkCompleteHeaderTableViewCell.swift
@@ -9,18 +9,30 @@ import UIKit
 
 class HomeWorkerPublicWorkCompleteHeaderTableViewCell: UITableViewCell {
 
+    @IBOutlet var completePeopleViewHeight: NSLayoutConstraint!
     @IBOutlet var personView: UIView!
     @IBOutlet var countLabel: UILabel!
+    @IBOutlet var completePeopleView: UIView!
+    @IBOutlet var tableView: UITableView!
     @IBOutlet var peopleCountLabel: UILabel!
+    var cellCount = 3
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        
+        completePeopleView.isHidden = true
         // view 클릭 시, 함수 정의
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(personViewTapped))
         personView.addGestureRecognizer(tapGestureRecognizer)
+        completePeopleViewHeight.constant = CGFloat(45 + 36 * cellCount)
+        setTableView()
     }
-
+    func setTableView(){
+        
+        tableView.register(UINib(nibName: "HomeWorkerCompletePeopleTableViewCell", bundle: nil),
+                           forCellReuseIdentifier: "HomeWorkerCompletePeopleTableViewCell")
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
@@ -28,5 +40,34 @@ class HomeWorkerPublicWorkCompleteHeaderTableViewCell: UITableViewCell {
     }
     @objc func personViewTapped(sender: UITapGestureRecognizer) {
         print("몇명일까요")
+        completePeopleView.isHidden.toggle()
     }
+}
+extension HomeWorkerPublicWorkCompleteHeaderTableViewCell: UITableViewDataSource,UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return cellCount
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            if let cell = tableView.dequeueReusableCell(withIdentifier: "HomeWorkerCompletePeopleTableViewCell") as? HomeWorkerCompletePeopleTableViewCell {
+                cell.selectionStyle = .none
+                
+                print(indexPath.row)
+                return cell
+            }
+        
+        return UITableViewCell()
+    }
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat{
+        
+            return 36
+        
+        
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("선택된 행은 \(indexPath.row) 입니다.")
+    }
+    
 }
