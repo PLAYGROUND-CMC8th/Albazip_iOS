f373a5400544cef657dd4eb36c8578069f1953f5
soobin-k <tnqls27qks@naver.com>
2021-11-28 16:30:54 +0900

    Add Connection Setting > UserPermission UI

--- Albazip/Source/Main/Setting/VC/SettingUseTermVC.swift
+++ Albazip/Source/Main/Setting/VC/SettingUseTermVC.swift
@@ -7,10 +7,33 @@
 
 import Foundation
 class SettingUseTermVC: UIViewController{
+    @IBOutlet var usePermission: UIStackView!
+    @IBOutlet var privatePermission: UIStackView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // view 클릭 시, 함수 정의
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(usePermissionTapped))
+        usePermission.addGestureRecognizer(tapGestureRecognizer)
+        
+        let tapGestureRecognizer2 = UITapGestureRecognizer(target: self, action: #selector(privatePermissionTapped))
+        privatePermission.addGestureRecognizer(tapGestureRecognizer2)
     }
     @IBAction func btnCacel(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
+    }
+    
+    @objc func usePermissionTapped(sender: UITapGestureRecognizer) {
+        let newStoryboard = UIStoryboard(name: "RegisterStoryboard", bundle: nil)
+        guard let nextVC = newStoryboard.instantiateViewController(identifier: "RegisterPermissionVC") as? RegisterPermissionVC else {return}
+        nextVC.status = 0
+        self.navigationController?.pushViewController(nextVC, animated: true)
+    }
+    
+    @objc func privatePermissionTapped(sender: UITapGestureRecognizer) {
+        let newStoryboard = UIStoryboard(name: "RegisterStoryboard", bundle: nil)
+        guard let nextVC = newStoryboard.instantiateViewController(identifier: "RegisterPermissionVC") as? RegisterPermissionVC else {return}
+        nextVC.status = 1
+        self.navigationController?.pushViewController(nextVC, animated: true)
     }
 }
